cb3fded9fc52009bb3c47d151d5eabb33aebd8c8
Илья <i.dontsov61rus@yandex.ru>
2023-09-29 12:35:31 +0300

    Update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import styles from "./Header.module.scss"
 
 export default function Header() {
@@ -6,10 +7,16 @@ export default function Header() {
       <nav className={styles.navigate}>
         <div className={styles.menu}>
           <h1>Navigate</h1>
-          <div className={styles.sub_menu}>Menu Menu Menu Menu Menu</div>
+          <div className={styles.sub_menu}>
+            <Link href="/" to="footer">
+              {" "}
+              Menu
+            </Link>
+            Menu Menu Menu Menu Menu
+          </div>
         </div>
       </nav>
-      <div className={styles.title}>
+      <div className={styles.title} id="travel">
         <h1>Travel</h1>
       </div>
     </header>
